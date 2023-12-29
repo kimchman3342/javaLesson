315708292269a29a4c40824b3a9ca0d9df56af2b
Hoony <qudgns0250@gmail.com>
2023-12-29 17:54:02 +0900

    과제 소스

--- App/src/myexam/HomeStringFormat.java
+++ App/src/myexam/HomeStringFormat.java
@@ -1,5 +1,15 @@
 package myexam;
 
 public class HomeStringFormat {
-    
+    public static void main(String[] args) {
+        // string.format() 사용한 예제 만들기
+        // 문제 : 
+
+        char gogo = 'A';
+
+        System.out.println(String.format(%d,gogo));
+
+        double intiger = 63.1515;
+        
+    }
 }

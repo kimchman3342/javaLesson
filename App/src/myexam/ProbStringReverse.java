69f1f9560951116c544120e9f38b746ec9b9ba63
Hoony <qudgns0250@gmail.com>
2024-01-03 17:46:17 +0900

    과제 소스

--- App/src/myexam/ProbStringReverse.java
+++ App/src/myexam/ProbStringReverse.java
@@ -7,7 +7,7 @@ package myexam;
 
 public class ProbStringReverse {
     public static void main(String[] args) {
-        String message = "Hello World";
+        String message = "Hello World";     // length : 11 , index : 0 ~ 10
         char[] messageArray = new char[message.length()];
 
         for(int i = messageArray.length-1; i >= 0; i-- ) {
@@ -16,9 +16,26 @@ public class ProbStringReverse {
             messageArray[message.length()-1-i] = temp;
         }
         
-        System.out.print("\nHello World 거꾸로 뒤집기 ♻♻♻  ");
+        
         System.out.print(messageArray);
         System.out.println(" 😀");
+
+        
+        
+        
+        
+        // 다른 방법
+        int cidx = message.length() - 1;    //10
+        for(int i = 0; i < message.length(); i++){
+            char temp = message.charAt(i);     // i = 0 일 때
+            messageArray[cidx] = temp;         // cidx = 10입니다.
+            // i = 1 일 때, cidx = 9       
+            // i = 2 일 때, cidx = 8
+            cidx--; //cidx 변수 없이 사용할 공식 찾아보세요.
+        } 
+         
+        System.out.print("\nHello World 거꾸로 뒤집기 ♻♻♻  ");
+        System.out.println(messageArray);
         
     }
     

325b560ba629a319eca7d9d13f0585fce9f7ca4a
Hoony <qudgns0250@gmail.com>
2024-01-03 15:09:24 +0900

    day 4

--- App/src/basic/day4/A26Test25.java
+++ App/src/basic/day4/A26Test25.java
@@ -1,5 +1,7 @@
 package basic.day4;
 
+import java.util.Scanner;
+
 public class A26Test25 {
     public static void main(String[] args) {
         // A25클래스에 만든 메소드를 사용할 수 있습니다.
@@ -8,6 +10,18 @@ public class A26Test25 {
 
         // 메소드가 private로 정의되면 다른 클래스에서 사용 못합니다.
         // A25MySampleMethod.checkJumsu(67);   
+
+        // 자바 (또는 다른 프로그래밍언어)로 코딩할 때
+        // 1. 자바에서 제공하는 클래스와 메소드를 잘 활용하기
+        // 2. 필요에 따라 여러분은 개발자가 직접 잘 만들어서 사용하기
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("시작 값을 입력하세요. >>> ");
+        int start = sc.nextInt();
+        System.out.print("마지막 값을 입력하세요");
+        int end = sc.nextInt();
+
+        System.out.println(start + "~" + end + " 까지의 곱하기 결과 : " + A25MySampleMethod.multiplyMToN(start, end));
     }
     
 }

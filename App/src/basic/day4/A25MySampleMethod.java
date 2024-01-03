74f7f5f78d0d75646a2866041899c7d4de528f27
Hoony <qudgns0250@gmail.com>
2024-01-03 11:53:25 +0900

    day 4

---
+++ App/src/basic/day4/A25MySampleMethod.java
@@ -0,0 +1,12 @@
+package basic.day4;
+// A25 소스파일은 구글 드라이브에 올려주세요.
+
+public class A25MySampleMethod {
+    // A22번 1) ~ 5) 을 각각 메소드로 정의해보기
+        // 1) sum1ToN   2) multiply1ToN 3) sumMToN  4) multiplyMToN 5)multiplyNof2
+        // 6) 구구단 '출력'하는 메소드. 리턴은 void.printGuguDan
+
+    public static void main(String[] args) {
+            }
+    
+}

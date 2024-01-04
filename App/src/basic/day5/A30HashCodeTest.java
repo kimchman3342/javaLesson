56ed3040f052b85227ef9bdf870798301fd576e3
Hoony <qudgns0250@gmail.com>
2024-01-04 10:56:48 +0900

    day 5

--- App/src/basic/day5/A30HashCodeTest.java
+++ App/src/basic/day5/A30HashCodeTest.java
@@ -13,10 +13,21 @@ public class A30HashCodeTest {
         iArray[3] = 99;
         System.out.println("\n4. 결과 >>>" + iArray.hashCode() + "(배열 요소 값을 변경한 후 해시코드)");
         System.out.println("\n결론 : 배열 값이 변경되어도 메모리의 위치는 동일");
+        System.out.println("-----------------------------------------------------");
 
-        System.out.println("\n String 배열 message의 해시코드 값 : " + message.hashCode());
+
+        System.out.println("\n5. String 배열 message의 해시코드 값 : " + message.hashCode());
         message = message + "~Good";
-        System.out.println("\n message를 변경한 후 해시코드 값 : " + message.hashCode());
+        System.out.println("\n6. message를 변경한 후 해시코드 값 : " + message.hashCode());
+        System.out.println("\n결론 : 문자열 String은 내용이 변경되면 메모리의 위치가 바뀝니다.");
     }
     
 }
+
+/*
+ * 객체란 ? 여러 종류의 데이터가 모여있는 데이터 덩어리.(기본형은 데이터의 최소단위)
+ * 객체는 참조형 변수를 사용합니다. (변수가 주소를 저장한다)
+ * 객체를 메모리에 저장했어요 ----> 초기에 저장한 데이터를 바꿀 수 있다.(배열) : 가변객체
+ *                          ----> 초기에 저장한 데이터를 바꿀 수 없다.(String) : 불변객체
+ * 
+ */

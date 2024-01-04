8bcee465eef0c38b7e3c8ca33284dfe0af819cba
Hoony <qudgns0250@gmail.com>
2024-01-04 17:50:55 +0900

    day 5

--- PartB/src/object/day5/Score.java
+++ PartB/src/object/day5/Score.java
@@ -3,14 +3,14 @@ package object.day5;
 import java.util.Arrays;
 
 public class Score {
-
+    // 인스턴스 필드
     private String name;    // 이름
     private int grade;      // 학년
     private int[] jumsues;  // 점수들이 저장될 배열
                             // 배열의 크기는 학년마다 다를 수 있음.
 
 
-
+    // 인스턴스 메소드 : 인스턴스 필드를 사용하는 처리를 합니다    (get부터 입력하면 순식간에 끝남)
     //getter
     public String getName(){
         return name;
@@ -24,6 +24,7 @@ public class Score {
         return jumsues;
     }
 
+    
 
     //setternayeon
     public void setName(String na){
@@ -64,4 +65,11 @@ public class Score {
         System.out.println("\n이름 = " + name + ", 학년 = " + grade + ", 점수 : "+ Arrays.toString(jumsues));
     }
 
+    /*
+     * public void printScore()
+     *      System.out.prinln("총점 : " + sum())
+     *      System.out.prinln("평균 : " + average())
+     *      System.out.prinln("과목개수 : " + jumses.length())
+     * 
+     */
 }

4c4f7ea5f8f68faa6c6a75615fdd8f544fadf0bb
Hoony <qudgns0250@gmail.com>
2024-01-05 17:51:12 +0900

    과제 소스2

--- PartB/src/object/myexam2/AddQuiz.java
+++ PartB/src/object/myexam2/AddQuiz.java
@@ -1,5 +1,29 @@
 package object.myexam2;
 
 public class AddQuiz {
+
+    private int data1;
+    private int data2;
+    private int userAnswer;
+    
+
+    //getter
+    public int getData1() {
+        return data1;
+    }
+
+    public int getData2() {
+        return data2;
+    }
+
+    public int getUserAnswer() {
+        return userAnswer;
+    }
+
+    
+    
+    
+    
+    //setter
     
 }
--- PartB/src/object/myexam2/AddQuizMain.java
+++ PartB/src/object/myexam2/AddQuizMain.java
@@ -1,5 +1,15 @@
 package object.myexam2;
 
 public class AddQuizMain {
+
+    public static void main(String[] args) {
+        AddQuiz an1 = new AddQuiz();
+        AddQuiz an2 = new AddQuiz();
+        AddQuiz usan = new AddQuiz();
+
+
+        System.out.println("덧셈 10문제 퀴즈를 시작합니다.");
+        System.out.println("1번 ." + an1.getData1());
+    }
     
 }

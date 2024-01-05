bd8563c04ce31ad346dda8a5e29dec4bd3ebc8b9
Hoony <qudgns0250@gmail.com>
2024-01-05 15:16:47 +0900

    day 5

--- PartB/src/object/day5/practiceMain1.java
+++ PartB/src/object/day5/practiceMain1.java
@@ -4,7 +4,7 @@ public class practiceMain1 {
     public static void main(String[] args) {
         
     }
-    
+  /*   
     //날짜
     Practice1 day = new Practice1();
 
@@ -14,7 +14,7 @@ public class practiceMain1 {
     // 공부한 내용
     day.setDay();
 
-
+ */
 
 
 }

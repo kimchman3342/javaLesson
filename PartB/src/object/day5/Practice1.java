54b6f626046249146a6fb12869207de28eb62150
Hoony <qudgns0250@gmail.com>
2024-01-05 10:45:40 +0900

    day 5

---
+++ PartB/src/object/day5/Practice1.java
@@ -0,0 +1,30 @@
+package object.day5;
+
+public class Practice1 {
+
+    private int day;
+    private String menu;
+
+
+    // getter
+    public int getDay() {
+        return day;
+    }
+
+    public String getmenu() {
+        return menu;
+    }
+
+    // setter
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public void setmenu(String menu) {
+        this.menu = menu;
+    }
+
+    // printDay()
+    
+    
+}
---
+++ PartB/src/object/day5/practiceMain1.java
@@ -0,0 +1,20 @@
+package object.day5;
+
+public class practiceMain1 {
+    public static void main(String[] args) {
+        
+    }
+    
+    //날짜
+    Practice1 day = new Practice1();
+
+
+
+
+    // 공부한 내용
+    day.setDay();
+
+
+
+
+}

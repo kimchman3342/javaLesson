edfff57406199c0449ea64cd5f6ae964c2c1a336
Hoony <qudgns0250@gmail.com>
2024-01-05 15:08:14 +0900

    day 6

--- PartB/src/object/day6/Product.java
+++ PartB/src/object/day6/Product.java
@@ -5,10 +5,14 @@ public class Product {
     private String productName;
     private int price;
     private String company;
+    private String[] etc;
 
     // 커스텀 생성자 만들기
-    public Product() {
-        
+    public Product(String productName,int price, String company, String[] etc) {
+        this.productName = productName;
+        this.price = price;
+        this.company = company;
+        this.etc = etc;
     }
     
 
@@ -23,5 +27,9 @@ public class Product {
 
     public String getCompany() {
         return company;
+    }
+
+    public String[] getEtc() {
+        return etc;
     }
 }

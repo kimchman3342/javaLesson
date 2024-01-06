2e8cf488ce7311d2fbc94d386bdb443eb59dad4b
hoony <qudgns0250@gmail.com>
2024-01-06 16:03:51 +0900

    과제 소스2

--- PartB/src/object/myexam2/AddQuizMain.java
+++ PartB/src/object/myexam2/AddQuizMain.java
@@ -11,10 +11,10 @@ public class AddQuizMain {
         
         
         AddQuiz quiz = new AddQuiz();
-        int[] an1 = new int[10];
-        int[] an2 = new int[10];
-        int[] usan = new int[10];
-        boolean[] right = new boolean[10];
+        int[] an1 = new int[5];
+        int[] an2 = new int[5];
+        int[] usan = new int[5];
+        boolean[] right = new boolean[5];
 
         
 
@@ -22,7 +22,8 @@ public class AddQuizMain {
         
         
 
-        System.out.println("\n덧셈 10문제 퀴즈를 시작합니다.");
+        System.out.println("\n📖 덧셈 5문제 퀴즈를 시작합니다. 📖\n");
+        System.out.println("----------------------------------");
         
         int start = 11; int end = 99; 
         
@@ -31,8 +32,9 @@ public class AddQuizMain {
             int temp2 = (int)(Math.random()*(end-start+1)) + start;
             
            
-            System.out.println(String.format("\n%d번) %d+%d= ?",(i+1),temp1,temp2));
-            System.out.print("답을 입력하세요. >>>");
+            System.out.println(String.format("\n%d번. %d + %d = ?",(i+1),temp1,temp2));
+            System.out.print("답을 입력하세요. >>> ");
+            
 
             quiz.setData1(temp1);
             quiz.setData2(temp2);
@@ -46,17 +48,27 @@ public class AddQuizMain {
         }
        
 
-            System.out.println("::::: 채점하고 있습니다.:::::\n");
+            System.out.println("\n::::: 채점하고 있습니다.:::::\n");
 
-            System.out.println("\n::::: 맞은 개수 " + "입니다.:::::\n");
-            System.out.println("\t문제\t제출한 답\t정답\t채점");
+            
+            System.out.println("<결과>  문제\t제출한 답\t정답\t채점");
+            int count = 0;
 
             for(int i = 0; i < usan.length; i++){
                 System.out.print(String.format("\n%d번. \t%d+%d\t%d\t\t%d", i + 1, an1[i], an2[i],usan[i],(an1[i]+an2[i])));
-                if(right[i] == true)System.out.println("\t⭕️");
-                else System.out.println("\t❌");
+                if(right[i] == true){
+                    System.out.println("\t⭕️");
+                    count++;
+                }else System.out.println("\t❌");
             }
-        
+            System.out.println("\n::::: 맞은 개수는 " + count + "개 입니다.:::::");
+            if(count == usan.length){
+                System.out.println(":::: 🐥🐥🐥 축하합니다. 만점입니다!!! 🐥🐥🐥 :::\n");
+            }else if(count == 0) {
+                    System.out.println("....? 반드시공부하세요...!! 😡\n");
+                }
+            
+            System.out.println("::: 수고하셨습니다!! ::::\n");
     }
     
 }

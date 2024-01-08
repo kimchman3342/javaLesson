62fcf4df5c3292cf126c2f92f9a37adf6eb6996f
Hoony <qudgns0250@gmail.com>
2024-01-08 11:13:55 +0900

    day 7

--- PartB/src/object/day7/B09ShapeMain.java
+++ PartB/src/object/day7/B09ShapeMain.java
@@ -5,6 +5,16 @@ package object.day7;
 public class B09ShapeMain {
     public static void main(String[] args) {
         
+        // 클래스를 불러와 변수로 만드는 작업
+        Shape sha = new Shape("모양", 30, 30);
+        Triangle tri = new Triangle("삼각형", 60, 30, 30);
+        
+        // 만든 변수를 클래스 내에 있는 메소드를 재료로 사용
+        sha.calculateArea();    
+        sha.print();
+        
+        tri.print();
+        tri.printAngle();
     }
     
 }

c42b9c316623f0f88b9647ae2bbe758509f9bc5a
Hoony <qudgns0250@gmail.com>
2024-01-08 17:46:22 +0900

    day 7

--- PartB/src/object/day7/review.java
+++ PartB/src/object/day7/review.java
@@ -1,9 +1,46 @@
 package object.day7;
 
-public class review {
 
-private
 
+    public class review {
+    
+        public class  A {
+        
+            
+        int a = 10;
+        public review(){
+            System.out.println("가");
+        }
+        public class review(int x){
+            
+        }
+        }
+    }
+
+
+
+        /**
+         * @param args
+         */
+        public static void main(String[] args) {
+            int a = 20;
+            B b1 = new B();
+            review b2 = new B(1);
+            System.out.println(b1.a + b2.a);
+                
+        
+        public class B extends review{
+            int a = 20;
+            public B(){
+                System.out.println("다");
+            }
+            public B (int x){
+                System.out.println("라");
+            }
+        
+            
+        }
+    }
     
      
-}
+

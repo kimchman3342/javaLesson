90cef705aa9292f06798deab046670f850e8de35
Hoony <qudgns0250@gmail.com>
2024-01-08 11:29:43 +0900

    day 7

--- PartB/src/object/day7/B09ShapeMain.java
+++ PartB/src/object/day7/B09ShapeMain.java
@@ -6,15 +6,28 @@ public class B09ShapeMain {
     public static void main(String[] args) {
         
         // 클래스를 불러와 변수로 만드는 작업
-        Shape sha = new Shape("모양", 30, 30);
-        Triangle tri = new Triangle("삼각형", 60, 30, 30);
+        Shape sha = new Shape("그냥도형", 20, 15);
+        Triangle tri = new Triangle("세모", 30, 20, 60);
         
-        // 만든 변수를 클래스 내에 있는 메소드를 재료로 사용
-        sha.calculateArea();    
+        // 만든 변수를 클래스 내에 있는 메소드를 재료로 사용     
+        System.out.println("~~~~~~~~재정의 메소드 확인~~~~~~~~~~");
+        System.out.println(sha.getShapeName() + "의 넓이 : " + sha.calculateArea());
+        System.out.println(sha.getShapeName() + "의 넓이 : " + tri.calculateArea());
+        
+        System.out.println("~~~~~~~~~~~~부모 클래스가 정의한 메소드 실행확인~~~~~~~~~~~~");
         sha.print();
+        tri.print(); // 재정의는 불가능 함.
         
-        tri.print();
-        tri.printAngle();
+
+        // getWidth, getHeight는 부모클래스 Shape에서 정의했음. 자식이 실행 가능.
+        System.out.println("세모 너비 : " + tri.getWidth());
+        System.out.println("세모 높이 : " + tri.getHeight());
+        System.out.println("~~~~~~~자식 클래스에서 정의한 메소드 실행확인~~~~~~~");
+        System.out.println("세모 각도 : " + tri.getAngle());
+        // sha.getAngle(); - 부모가 자식객체의 메소드 실행 못함.(자식한테 선택권이 있음)
+
+        // 상속에서의 변수 참조 타입 - 자식클래스 타입, 부모클래스 타입.
     }
     
 }
+ 

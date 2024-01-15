48ec8d84fd6600071649fe8d88debf4480ace666
Hoony <qudgns0250@gmail.com>
2024-01-15 16:52:41 +0900

    io.day 12

---
+++ PartC/src/io/day12/StandardIOTest.java
@@ -0,0 +1,30 @@
+package io.day12;
+
+import java.io.IOException;
+
+// 표준 입출력 키보드 테스트
+// 키보드 입력 : new Scanner(System.in) 또는 System.console().readLine()
+public class StandardIOTest {
+    
+    public static void main(String[] args) {
+        int b;
+        System.out.println("입력하세요. ↓");
+        // 표준입출력은 모든 글자가 2바이트. = > 윈도우 터미널 인코딩
+        try {
+            int count = 0;
+            while ((b = System.in.read()) != -1) {  // -1(입력 끝)은 ctrl + Z
+                
+                // System.out.println(b);   // 문자 인코딩 안하고 정수 값 출력
+                // System.in.read(); 로 입력 받은 정수 값을 문자로 인코딩하는 메소드
+                System.out.write(b);    
+                count++; 
+            }
+            System.out.println("총 입력 바이트 수 : " + count);
+                
+            
+           
+        }catch (IOException e) {
+            System.out.println("예외 : " + e.getMessage());
+        }
+    }
+}
--- PartC/src/io/day12/TryCatchTest.java
+++ PartC/src/io/day12/TryCatchTest.java
@@ -4,13 +4,19 @@ import java.util.Scanner;
 
 public class TryCatchTest {
     public static void main(String[] args) {
-        // 예외 처리 구문 : 비정상적인 종료에 대해 사용자에게 전달.    
+        // 예외 처리 구문 : 비정상적인 종료에 대해 사용자에게 전달. 또는 이후의 명령을 계속 실행하도록 합니다.
+        // NumberFormatException, ArrayIndexOutOfBoundsException 은 예외처리가 강제가 아닙니다.
+        // 언체크 익셉션
         try {
         // 예외 가능성이 있는 명령문 작성하기
+        indexException();   // numberException();
        } catch (NumberFormatException e)  {  // (인자) : 처리할 예외의 종류 : Exception은 모든 예외.
         // 예외가 발생할 때 실행할 명령
         System.out.println("정수는 숫자만 입력하세요.");
-       } 
+       } catch (ArrayIndexOutOfBoundsException e)  {  
+        System.out.println("인덱스 범위는 배열의 길이 -1 까지입니다.");
+       }
+       System.out.println("마지막 명령어 입니다."); 
     }
     
     
@@ -21,12 +27,15 @@ public class TryCatchTest {
         System.out.println(arrays[arrays.length]);
     }
     
-    public static void IndexException() {
+    public static void numberException() {
         Scanner sc = new Scanner(System.in);
         System.out.println("정수를 입력하세요. ");
         int number = Integer.parseInt(sc.nextLine());
         System.out.println("입력한 값 : " + number);
+        sc.close();
     }
+
+    
 
 
 }

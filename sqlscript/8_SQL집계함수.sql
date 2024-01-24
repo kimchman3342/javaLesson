32738311ab419a4e6762a25b6289cd9ef491690e
Hoony <qudgns0250@gmail.com>
2024-01-24 13:28:29 +0900

    sql day 2

--- sqlscript/8_SQL집계함수.sql
+++ sqlscript/8_SQL집계함수.sql
@@ -43,4 +43,39 @@ SELECT SUBJECT, COUNT(*) FROM TBL_SCORE ts GROUP BY SUBJECT ;
 -- GROUP BY STUNO 구문이 있으므로 조회 컬럼에 STUNO를 추가할 수 있습니다.
 SELECT STUNO , COUNT(*) FROM TBL_SCORE ts GROUP BY STUNO  ; 
 
+/*
+ * 집계함수 count, avg , sum, min, max 등 그룹 함수라고도 합니다. 
+ * 그룹화 - 지정한 컬럼값이 같을 것을 갖는 행에 대해 집계 함수를 실행합니다.
+ *      
+ * 
+ * select 
+ *       그룹화컬럼, 그룹함수
+ * from 테이블이름
+ * [WHERE] 그룹화하기 이전의 조건식
+ * [GROUP BY] 그룹화에 사용할 컬럼명
+ * [HAVING] 그룹화 후에 그룹화 결과 값을 사용하는 조건식
+ * [ORDER BY] 정렬 컬럼
+ */
+
+
 -- GROUP BY는 그룹화컬럼명 1, 그룹화컬럼명 2 ,,....로 할 수 있습니다.
+
+-- 예시 1 : 학번(학생) 별로 수강한 갯수와 평균을 구해보세요.
+SELECT STUNO, COUNT(*), AVG(jumsu)
+FROM TBL_SCORE ts 
+GROUP BY STUNO; 
+
+
+-- 예시 2 : 위의 결과에 대해 평균이 80점 미만인 학번(학생)을 조회해 보세요.
+SELECT stuno, COUNT(*),AVG(jumsu)
+FROM TBL_SCORE ts 
+GROUP BY STUNO 
+HAVING AVG(JUMSU) < 80;		-- having 은 집계함수 결과에 대한 조건식입니다.
+							
+-- 예를 들어서 점수가 80점 이상 데이터만 예시 1번을 실행하도록 한다면 where jumsu >= 80 group by stuno
+SELECT STUNO, COUNT(*), AVG(jumsu)
+FROM TBL_SCORE ts 
+WHERE JUMSU >= 80
+GROUP BY STUNO; 
+
+-- 예시 3 : 예시 1번의 결과를 평균 내림차순으로 조회하세요.

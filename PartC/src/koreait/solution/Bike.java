1954b31cbf0f7080539879c43e4956e791d49d89
Hoony <qudgns0250@gmail.com>
2024-01-25 10:39:09 +0900

    solution

---
+++ PartC/src/koreait/solution/Bike.java
@@ -0,0 +1,92 @@
+package koreait.solution;
+
+// 0) 객체의 구성 요소 : 인스턴스 필드와 인스턴스 메소드
+//    클래스 구성 요소 : 객체의 구성 요소 + static 메소드와 필드
+//    클래스는 객체가 만들어지는 인스턴스 요소를 정의하는 상위 개념.
+public class Bike extends Product{
+    // 4) 필드
+    private int speed;
+
+    // 3)
+    public Bike(int price, String prdName, int speed) {
+        // super();  생략가능
+        // 자식 클래스는 부모 생성자 반드시 호출한다.         
+        this.price = price;
+        this.prdName = prdName;
+        // price 와 prdName 은 protected 필드이므로 자식은 직접 접근 가능합니다.
+        this.speed = speed;
+    }
+
+    // 8) 추상클래스 상속 받았으므로 반드시 추상메소드 구현(재정의)
+    @Override
+    public String sell(Object object) {
+        // TODO Auto-generated method stub
+        return String.format("[ %s] 행사 - %d %% 인하", this.prdName, object);
+    }
+
+
+    // 4)
+    public int getSpeed() {
+        return speed;
+    }
+   
+
+    // 4)
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    // 5) getter,setter, ride 메소드 : 인스턴스 필드를 대상으로 처리 기능을 갖습니다. 인스턴스 메소드
+    //                                 객체 = 인스턴스
+    public String ride(){
+        // this 생략 가능하나 speed가 객체의 필드 변수라는 것을 가독성 있게 해줍니다.
+       return "당신은 이 것을 시속" + this.speed + "km로 탈 수 있습니다.";
+    }
+
+    // 10)
+    @Override
+    public String toString() {
+        return "Bike [" + super.toString() + ", speed=" + speed + "]";
+    }
+
+    
+
+}
+
+class Electronics extends Product{
+    private double kwh; // 필드 : 클래스 소속된 변수. 클래스 전역 변수 
+
+    // 3) price, prdName 필드 값만 초기화 - 생성자
+    public Electronics(int price, String prdName) {
+        super();
+        this.price = price;
+        this.prdName = prdName;
+    }
+   
+    // 문제 2에서 필요함
+    public void setKwh(double kwh) {
+        this.kwh = kwh;
+    }
+
+
+
+    // 9)
+    @Override
+    public String sell(Object object) {
+        // TODO Auto-generated method stub
+        return String.format("[ %s] 증정 - %s", this.prdName, object);
+    }
+
+    // 7)
+    public double power(){
+        return this.kwh*24;
+    }
+
+    
+    // 10) Product 클래스에서 toString 재정의 한 것을 자식클래스가 또(다시) 정의
+    @Override
+    public String toString() {
+        return "Electronics [" + super.toString() + ", kwh=" + kwh + "]";
+    }
+
+}
---
+++ PartC/src/koreait/solution/ProductApp.java
@@ -0,0 +1,66 @@
+package koreait.solution;
+
+public class ProductApp {
+    public static void main(String[] args) {
+        
+        // 2-1)
+        Product[] cart = new Product[10];
+        
+        cart[0] = new Bike(123000,"MTB",25);
+        cart[3] = new Bike(99000,"삼천리",15);
+        cart[1] = new Electronics(35000,"USB");
+        cart[5] = new Electronics(527000,"스마트TV");
+        cart[7] = new Electronics(2250000,"lg냉장고");
+        
+        // 2) power 메소드 : Electronics 자식 클래스의 메소드 
+        //           현재 부모타입으로 참조하는 것을 자식타입으로 변경
+        if(cart[5] instanceof Electronics){
+        Electronics tv = (Electronics) cart[5];
+        tv.setKwh(0.9);
+        System.out.println(tv.power());
+        }else
+            System.out.println("Electronics 타입으로 변경 불가.");
+        
+        
+        
+            // 3) 배열 10개 중에 객체가 참조된 것은 5개
+        // -> 반복문으로 실행할 떄 반드시 null이 아닌 것만 하도록 합니다.
+        // -> null 이면 메소드 실행 예외 발생합니다. NullpointerException 
+        for(Product p : cart) {
+        if(p!= null && p.price >= 100000) {
+            // System.out.println(p.toString());
+            System.out.println(p);      // toString() 메소드이름 생략 가능
+            }
+         }
+    
+  
+         // 4) ride 메소드 : Bike 자식 클래스의 메소드 
+         //   현재 부모타입으로 참조하는 것을 자식 타입으로 변경.(다운캐스팅) 
+         for(Product p : cart) {
+            if(p!= null && p instanceof Bike) {
+                Bike bike = (Bike) p;
+                System.out.println(bike.ride());
+         }
+     }
+
+    //  5)
+    System.out.println("\n문제5.");
+    String result = cart[3].sell(20);
+    System.out.println(result);
+
+    // 6)
+    System.out.println("\n문제6.");
+    System.out.println(cart[5].sell("사운드바"));
+ }
+}
+
+/* 자바 교재 204 페이지
+ * 라이브러리 클래스 : Product, Bike, Electronics
+ *                   ㄴ 프로그래밍에 필요한 재료로 사용되는 클래스
+ *                   ㄴ 인스턴스 메소드와 static 메소드로 제공되는 기능을 사용함.
+ * 실행 클래스 : ProductApp
+ *                  ㄴ main 메소드를 포함하여 프로그램을 실행함.
+ *                  ㄴ main 메소드는 반드시 static 이어야 합니다.
+ *                  ㄴ main 메소드를 위한 객체를 먼저 생성할 수 없다. 그래서 static 입니다.  
+ * 
+ */

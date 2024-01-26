35c0b77742fc61467ff151f5db592222225b01ae
Hoony <qudgns0250@gmail.com>
2024-01-26 14:51:32 +0900

    sql day 5

--- sqlscript/19_프로시저.sql
+++ sqlscript/19_프로시저.sql
@@ -1,254 +1,82 @@
-/* 	SQL은 2월 1일에 SELECT 10문제
- *  팀별 예시 문제 : 출제 1월 25일
- 				: 이런 것은 '비즈니스 로직에 필요하다'
- 	요구사항 : 고객은 custom 테이블에, 상품은 product 테이블에 저장된 값만 buy 테이블에 사용 가능.
- 			구매는 '동일한 고객이 동안한 상품을 여러번 구매할 수 있다.' => PK를 idx 컬럼으로 추가함.
- * */
-
--- A조
---김미나가 구매한 상품 금액 합계(이광원)
-SELECT CUSTOMID  ,SUM(PRICE * QUANTITY) "구매 금액"
-FROM  TBL_PRODUCT tp
-JOIN  TBL_BUY tb ON tp.PCODE = tb.PCODE
-AND CUSTOMID ='mina012'
-GROUP BY CUSTOMID  ;
-
-
---이름에 '길동'이 들어가는 회원의 구매한 상품의 구매현황 조회?(권태윤)
-SELECT NAME ,SUM(QUANTITY) "상품 갯수"
-FROM  TBL_CUSTOM tc 
-JOIN  TBL_BUY tb ON tc.CUSTOM_ID = tb.CUSTOMID 
-AND NAME ='홍길동'
-GROUP BY NAME ;
-
--- 수정
-SELECT PCODE, tb.QUANTITY, tb.BUY_DATE 
-FROM TBL_CUSTOM tc 
-JOIN TBL_BUY tb 
-ON tc.CUSTOM_ID = tb.CUSTOMID 
-WHERE name = '홍길동';
-
--- 1) '20살'이상 고객님들의 '구매'한 '상품명'(강주찬)  => 2)테이블 2개 조인으로 문제를 변경하세요.
-SELECT tp.*
-FROM TBL_PRODUCT tp 
-JOIN TBL_BUY tb 
-ON tp.PCODE  = tb.PCODE 
-JOIN TBL_CUSTOM tc
-ON tb.CUSTOMID = tc.CUSTOM_ID
-WHERE tc.AGE > 20;
-
--- 수정
-SELECT CUSTOMID ,tc.AGE, tb.PCODE 
-FROM TBL_BUY tb 
-JOIN TBL_CUSTOM tc  
-ON tb.CUSTOMID = tc.CUSTOM_ID  
-WHERE tc.AGE > 20;
-
-
-
--- 사과를 구매한 사람의 이름과 구매한 갯수만큼의 가격의 합을 구해줘(고길현)
-SELECT tc.NAME, sum("구매 금액의 합") FROM 
-(SELECT NAME, tp.PRICE , tp.PRICE * tb.QUANTITY "구매 금액의 합" FROM TBL_CUSTOM tc JOIN TBL_BUY tb ON tc.CUSTOM_ID = tb.CUSTOMID JOIN TBL_PRODUCT tp ON tp.PCODE = tb.PCODE AND tp.PNAME = '청송사과 5kg')
-tc GROUP BY tc.NAME;
-
--- 총 구매합산 금액이 20000~70000사이 값을 출력하시오(김태완)
-SELECT SUM(price) 
-(SELECT * 
-FROM TBL_PRODUCT tp 
-JOIN TBL_BUY tb 
-ON tp.PCODE = tb.PCODE);
-
-SELECT * 
-FROM TBL_PRODUCT tp 
-JOIN TBL_BUY tb 
-ON tp.PCODE = tb.PCODE 
-
-
--- B조
--- 이대환 나이가 30이상인 회원들의 나이와 상품코드를 서브쿼리와 조인을 모두 사용해 조회하세요.
--- 정답 :
-SELECT abc.age, abc.pcode FROM (
-SELECT * FROM TBL_BUY tb
-JOIN TBL_CUSTOM tc ON tb.CUSTOMID = tc.CUSTOM_ID AND age>30) abc
-GROUP BY abc.age, abc.pcode;
-
--- 김승한 물건을 구매한 사람 중, 가장 나이가 많은 고객을 찾으려고 한다. 다음 빈칸에 맞는 코드를 넣으시오. (테이블이 되는 코드는 ‘13_9번문제.sql 파일을 기반으로 한다.’)
-SELECT tc.CUSTOM_ID ,tc.NAME , tc.AGE 
-FROM TBL_CUSTOM tc 
-JOIN TBL_BUY tb ON CUSTOM_ID = tb.CUSTOMID 
-WHERE tc.AGE =
-(SELECT MAX(age)
-FROM TBL_CUSTOM 
-);
-
--- 노희영 2021년 12월1일 부터 2022년 03월17일 사이에 가장 많이 산 사람의 이름 , 물건이름 , 수량순으로 조회하시오
-SELECT A.NAME "이름", C.PNAME "물건이름", MAX(B.QUANTITY) ||'개'"수량"
-FROM TBL_CUSTOM A, TBL_BUY B, TBL_PRODUCT C
-WHERE A.CUSTOM_ID = B.CUSTOMID
-AND B.PCODE = C.PCODE
-AND QUANTITY = (SELECT MAX(QUANTITY)
-FROM TBL_BUY
-WHERE BUY_DATE BETWEEN TO_DATE('2021-12-01','yyyy-mm-dd')
-AND TO_DATE('2022-03-17','yyyy-mm-dd'))
-GROUP BY A.NAME,C.PNAME ;
-
--- 이예진 회원별로 가장 많은 사용금액을 구하시오
-WITH BIG
-AS 
-(SELECT TB.PCODE, TB.CUSTOMID, MAX(tp.PRICE * tb.QUANTITY) AS "많이 쓴 금액"
-FROM TBL_PRODUCT tp
-JOIN TBL_BUY tb ON tp.PCODE = tb.PCODE
-GROUP BY TB.PCODE,TB.CUSTOMID)
-SELECT BIG.CUSTOMID, MAX("많이 쓴 금액") AS "하나"
-FROM BIG
-GROUP BY BIG.CUSTOMID;
-
--- 이재훈 구매이력이 2 이상인 고객 중에 나이가 20을 초과한 인원만 조회하세요.
-SELECT tc.CUSTOM_ID , tc.NAME , tc.EMAIL ,tc.AGE , tb.QUANTITY 
-FROM TBL_CUSTOM tc  
-LEFT JOIN TBL_BUY tb 
-ON tc.CUSTOM_ID = tb.CUSTOMID  
-WHERE tb.QUANTITY >=2 AND tc.AGE >20;  
-
--- 민찬희 tbl_student 와 tbl_score 에서 '김모모'라는 이름을 가진 2021001 학번의 점수를 각각 출력하고 평균을 구하시오.
-SELECT
-st.stuno 학생번호,
-st.name 이름,sc.JUMSU 점수,(SELECT avg(JUMSU) FROM tbl_score WHERE
-st.STUNO='2021001'),sc.SUBJECT 과목 FROM TBL_STUDENT st
-INNER JOIN TBL_SCORE sc ON st.stuno = sc.stuno
-WHERE st.name = '김모모';
+/*
+PL/SQL : Procedure(절차,순서) Language , 기존의 단순한 SQL이 확장된 언어(SQL로 만드는 프로그램)
+         변수,제어문(if,반복문)을 사용하여 프로그래밍언어와 같이 sql 실행의 흐름을 제어
+- 구성 : 선언부(DECLARE), 실행부(BEGIN), 예외처리부 (EXCEPTION)로 구성됨
+- 유형
+	1) Anonymous PL/SQL Block(익명블록) 
+	익명 블록은 주로 일회성으로 사용
+	2)Stored PL/SQL Block (저장 프로시저)
+	서버에 파싱해서 저장해 놓고 주기적으로 반복해서 사용할 경우 사용됨
+	서브프로그램 또는 프로그램 단위라고도 하며, 
+	스키마를 구성하는 오브젝트로서 파싱된 후 오라클 서버 내부에 저장되거나 오라클 툴 안에 라이브러리 형태로 저장되어 있음
  
+Declare (선언부) 
+Executable (실행부) 
+Exception (예외 처리부)
+*/ 
+SELECT * FROM TBL_CUSTOM tc ;
+-- 1) 익명 프로시저(일회용)
+DECLARE   -- 변수선언부  
+	-- vname varchar2(40);			 -- 스칼라변수
+	-- vage number(3,0);
+	vname tbl_custom.name %TYPE;     -- 참조변수(타입변수) : 테이블명.컬럼명 &TYPE
+	vage tbl_custom.age %TYPE;
+BEGIN		--프로시저 시작(실행부)
+  -- 프로시저 내부에는 주로 트랜잭션을 구성하는 여러개의 DML 명령문들을 작성
+	SELECT name,age 
+		INTO vname , vage	-- 프로시저 구문: 검색결과를 프로시저 일반 변수 vname, vage 에 저장
+	FROM "TBL_CUSTOM" tc 
+	WHERE CUSTOM_ID ='hong';		-- 1개 행만 결과 조회되는 조건
+									-- 여러개 행 조회될때는 각 행에 접근하기 위한 cursor 필요.
+-- DBMS_OUTPUT 는 콘솔에 출력하는 오라클 패키지의 하나이며 PUT_LINE 함수
+	DBMS_OUTPUT.PUT_LINE('고객이름 : ' || vname);   -- ||는 문자열 연결 연산
+	DBMS_OUTPUT.PUT_LINE('고객나이 : ' || vage);
+	EXCEPTION		-- 예외(오류)처리
+	WHEN no_data_found THEN   -- no_data_found : 예외 이름 (정해진 이름), OTHERS : 모든 예외
+		DBMS_OUTPUT.PUT_LINE('찾는 데이터가 없습니다.');
+END;		-- 프로시저의 끝
 
---C조
--- 문제 : 가격이 1만원 이상의 상품마다 각각 고객들이 구매한 개수의 평균을 출력하시오. (출제자 : 임현범)
-SELECT tp.PNAME "상품명", avg(tb.QUANTITY) "평균 구매 개수"
-FROM TBL_BUY tb 
-JOIN TBL_PRODUCT tp ON tb.PCODE = tp.PCODE
-WHERE tp.PRICE >= 10000
-GROUP BY tp.PNAME;
+/*
+	저장 프로시저 생성
+	CREATE OR REPLACE PROCEDURE 프로시저 이름
+	( 매개변수이름 [IN 또는 OUT] 데이터 타입,.....)	IN : 입력매개변수, OUT : 출력매개변수(리턴 값 저
+	[IS 또는 AS]
+		프로시저 변수 선언
+	BEGIN
+		프로시저 작업 내용 SQL들....
 
--- 문제 : 진라면을 구매한 고객의 이름, 구매수량, 구매날짜를 조회하자. (출제자 : 전예진)
-WITH bi 
-AS (
-    SELECT CUSTOMID, PCODE, QUANTITY "구매수량", BUY_DATE "구매날짜"
-    FROM TBL_BUY
-    WHERE PCODE = 'JINRMn5'
+	EXCEPTION 
+		WHEN 예외이름 THEN 처리 내용;
+
+	END ;
+
+
+*/
+
+-- 2) 1)번을 저장 프로시저로 변경했습니다.
+-- 인자와 리턴을 주는 형식. 저장프로시저 정의하기
+CREATE OR REPLACE PROCEDURE search_custom(   -- 프로시저 이름 설정
+      c_id IN tbl_custom.CUSTOM_ID %TYPE      -- 매개변수(인자) IN 
 )
-SELECT tc.NAME "고객명", bi.구매수량, bi.구매날짜
-FROM bi
-JOIN TBL_CUSTOM tc 
-ON bi.CUSTOMID = tc.CUSTOM_ID;
+IS 
+   vname tbl_custom.name %TYPE   -- 지정된 테이블의 컬럼과 동일형식의 변수
+   vage tbl_custom.age %TYPE
+BEGIN 
+   SELECT name,age 
+      INTO vname , vage   
+   FROM "TBL_CUSTOM" tc 
+   WHERE CUSTOM_ID =c_id;      -- 1개 행만 결과 조회되는 조건. 매개변수로 전달된 값으로 조건 실행
+   
+   DBMS_OUTPUT.PUT_LINE('고객이름 : ' || vname);  
+   DBMS_OUTPUT.PUT_LINE('고객나이 : ' || vage);
+   EXCEPTION      -- 예외(오류)처리
+   WHEN no_data_found THEN   
+      DBMS_OUTPUT.PUT_LINE('찾는 데이터가 없습니다.');   
+END;   
 
--- **문제 : '20대 사용자들이 구매한 가격의 평균가격'(출제자 : 조이루)
-SELECT tc.custom_id, tc.name, AVG(tp.price) AS avg_price
-FROM TBL_CUSTOM  tc 
-JOIN TBL_BUY tb ON tc.custom_id = tb.customId
-JOIN TBL_PRODUCT tp ON tb.pcode = tp.pcode
-WHERE tc.age BETWEEN 20 AND 29
-GROUP BY tc.custom_id, tc.name;
+-- 프로시저 실행
+BEGIN 
+	SEARCH_custom('mina012');
+END;
 
--- 문제 : 2023년에 팔린 상품의 이름과 코드,총 판매액 그리고 총 판매개수를 구하시오. (출제자 : 정제원)
-SELECT total.PCODE,PNAME, SUM(QUANTITY) "총 구매개수", SUM("총 가격")
-FROM (
-    SELECT tp.PCODE,PNAME, QUANTITY, QUANTITY * PRICE "총 가격", BUY_DATE  
-    FROM TBL_PRODUCT tp, TBL_BUY tb 
-    WHERE tp.PCODE = tb.PCODE AND to_char(BUY_DATE, 'yyyy') = '2023'
-) total
-GROUP BY total.PCODE, PNAME; -- 서브쿼리 없이 변경하세요.
-
--- 문제 : 미나와 길동이는 한집에 살고 있습니다. 미나와 길동이가 구매한 상품,수량,가격을 조회하세요. (출제나 : 장성우)
-WITH minaGD
-as(
-	SELECT *
-	FROM TBL_BUY tb 
-	WHERE CUSTOMID = 'twice' OR  CUSTOMID ='hongGD' 
-)
-SELECT tp.PNAME, minaGD.quantity,tp.PRICE 
-FROM minaGD
-JOIN TBL_PRODUCT tp
-ON minaGD.PCODE = tp.PCODE ;
-
-
---D조
--- 조하연 문제 : 진라면을 가장 많이 구매한 회원을 구매금액이 높은 순으로 회원아이디와 총 진라면 구매금액을 보여주세요.
-SELECT X.CUSTOMID, X.SUM
-FROM
-(
-SELECT tb.customid, tb.quantity, tp.price, tb.quantity * tp.price AS sum
-FROM TBL_PRODUCT tp,
-tbl_buy tb
-WHERE tp.pcode = tb.pcode
-AND tp.pcode = 'JINRMn5'
-) X
-ORDER BY SUM DESC;
-
--- 1) 조인 없는 서브쿼리 
--- 2) 조인만 한 쿼리
-
--- 한진만 문제 : '팔린 갯수가 가장 많은 순서'로 (상품코드로 그룹화)로 상품(상품코드와 상품이름)을 정렬하고 총 팔린 금액을 출력하시오
-SELECT tp.PCODE, tp.PNAME, tb.QUANTITY "팔린 갯수" , tb.QUANTITY * tp.PRICE "총 팔린 금액"
-FROM TBL_PRODUCT tp
-LEFT JOIN TBL_BUY tb ON tb.PCODE = tp.PCODE
-WHERE tb.BUY_IDX IS NOT NULL
-ORDER BY QUANTITY DESC ;
-
--- 수정
-SELECT tp.PRICE"총 팔린금액"
-FROM 
-	(SELECT tb.PCODE , tp.PNAME ,tb.QUANTITY ,tp.PRICE
-	FROM TBL_PRODUCT tp
-	LEFT JOIN TBL_BUY tb 
-	ON tp.PCODE = tb.PCODE)
-;
-
-	SELECT tb.PCODE , tp.PNAME ,tb.QUANTITY ,tp.PRICE
-	FROM TBL_PRODUCT tp
-	LEFT JOIN TBL_BUY tb 
-	ON tp.PCODE = tb.PCODE;
-
--- 황병훈 문제 : 진라면을 구매한 고객들의 평균 나이를 제품코드(PCODE)와 함께 출력해 주세요.
-SELECT PCODE ,AVG(AGE) "진라면 구매자의 평균 나이"
-FROM
-(SELECT AGE, PCODE
-FROM TBL_CUSTOM tc , TBL_BUY tb
-WHERE tc.CUSTOM_ID = tb.CUSTOMID
-AND PCODE = 'JINRMn5')
-GROUP BY PCODE;
-
---조지수 문제 : '30이하' 회원별 구매 금액('단가','수량')을 구하고 회원으로 그룹바이해서 구매금액 합계가 큰 순으로 정렬
-SELECT abc.CUSTOMID, sum(pay_sum) AS total
-FROM
-(
-SELECT tb.customid, tp.price * tb.quantity AS pay_sum
-FROM TBL_PRODUCT tp, TBL_buy tb
-JOIN TBL_CUSTOM tc ON tb.CUSTOMID = tc.CUSTOM_ID AND age<30
-WHERE tp.pcode = tb.pcode
-) abc
-GROUP BY abc.CUSTOMID
-ORDER BY total DESC;
-
--- 수정 (20개 중 10개)
-
--- 한주영 문제 2023년 구매 금액 5만원 이상에게 할인쿠폰을 발송하려 한다. CUSTOMID,구매 총액, 구매 일자를 구하시오. COUPON 사용하기
-SELECT COUPON.CUSTOMID "고객id",
-SUM(COUPON.TOTAL) "구매 총액"
-FROM (
-SELECT tb.QUANTITY * tp.PRICE 
-AS TOTAL, BUY_DATE , tb.CUSTOMID, QUANTITY,PRICE
-FROM TBL_PRODUCT tp, TBL_BUY tb
-WHERE tp.PCODE = tb.PCODE
-AND TO_CHAR(BUY_DATE,'YYYY')='2023'
-) COUPON
-GROUP BY COUPON.CUSTOMID
-HAVING SUM(COUPON.QUANTITY * COUPON.PRICE)>50000;
-
--- 차정호 문제 : 문제 햇반의 총 매출과 팔린 갯수 산사람의 회원 아이디를 구하세요
-SELECT dongwon.CUSTOMID, dongwon.Price1 "가격", dongwon.quantity "개수"
-FROM
-(
-SELECT tb.customid, tb.quantity, tp.price, tp.price* tb.quantity AS Price1
-FROM TBL_PRODUCT tp,
-tbl_buy tb
-WHERE tp.pcode = tb.pcode
-AND tp.pcode = 'CJBAb12g'
-)dongwon
+EXEC SEARCH_custom('hong');

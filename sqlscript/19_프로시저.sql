00e477364387b20e782260080ac5c34866b4963f
resetok <746aa1357@gmail.com>
2024-01-26 15:42:52 +0900

    sql day 5

--- sqlscript/19_프로시저.sql
+++ sqlscript/19_프로시저.sql
@@ -5,6 +5,7 @@ PL/SQL : Procedure(절차,순서) Language , 기존의 단순한 SQL이 확장�
 - 유형
 	1) Anonymous PL/SQL Block(익명블록) 
 	익명 블록은 주로 일회성으로 사용
+	
 	2)Stored PL/SQL Block (저장 프로시저)
 	서버에 파싱해서 저장해 놓고 주기적으로 반복해서 사용할 경우 사용됨
 	서브프로그램 또는 프로그램 단위라고도 하며, 
@@ -59,19 +60,19 @@ CREATE OR REPLACE PROCEDURE search_custom(   -- 프로시저 이름 설정
       c_id IN tbl_custom.CUSTOM_ID %TYPE      -- 매개변수(인자) IN 
 )
 IS 
-   vname tbl_custom.name %TYPE   -- 지정된 테이블의 컬럼과 동일형식의 변수
-   vage tbl_custom.age %TYPE
+   vname tbl_custom.name %TYPE ;  -- 지정된 테이블의 컬럼과 동일형식의 변수
+   vage tbl_custom.age %TYPE;
 BEGIN 
    SELECT name,age 
       INTO vname , vage   
-   FROM "TBL_CUSTOM" tc 
+   FROM "TBL_CUSTOM" tc ;
    WHERE CUSTOM_ID =c_id;      -- 1개 행만 결과 조회되는 조건. 매개변수로 전달된 값으로 조건 실행
    
    DBMS_OUTPUT.PUT_LINE('고객이름 : ' || vname);  
    DBMS_OUTPUT.PUT_LINE('고객나이 : ' || vage);
-   EXCEPTION      -- 예외(오류)처리
+   EXCEPTION      
    WHEN no_data_found THEN   
-      DBMS_OUTPUT.PUT_LINE('찾는 데이터가 없습니다.');   
+      DBMS_OUTPUT.PUT_LINE(CHR(10_'찾는 데이터가 없습니다.');   
 END;   
 
 -- 프로시저 실행
@@ -79,4 +80,42 @@ BEGIN
 	SEARCH_custom('mina012');
 END;
 
-EXEC SEARCH_custom('hong');
+-- EXEC SEARCH_custom('hong');	-- 디비버에서는 실행 안됩니다.
+
+-- 구매 수량이 최대인 고객의 이름, 나이 출력하는 프로시저 : max_custom
+CREATE OR REPLACE PROCEDURE max_custom(	   -- 자바의 메소드 인자와 같은 개념
+   p_name OUT tbl_custom.NAME %TYPE,      -- 출력(리턴) 변수
+   p_age OUT tbl_custom.AGE %TYPE
+)
+IS
+   maxval number(5);
+   cid tbl_custom.custom_id %TYPE;
+BEGIN
+   SELECT max(quantity)		-- 구매 수량 최대 값
+      INTO maxval 			-- 조회 결과를 변수에 저장
+   FROM tbl_buy; 
+
+   SELECT customid
+      INTO cid				-- 조회 결과를 변수에 저장
+   FROM tbl_buy
+   WHERE quantity = maxval;
+
+   SELECT name,age 
+      INTO p_name , p_age   -- 출력 매개변수에 저장
+   FROM "TBL_CUSTOM" tc 
+   WHERE CUSTOM_ID =cid;
+END;
+
+-- 프로시저 실행 : 출력 매개변수가 있는 프로시저
+DECLARE
+	vname tbl_custom.name %TYPE;
+	vage tbl_custom.age %TYPE;
+BEGIN
+	max_custom(vname,vage);		-- 프로시저 실행 결과 값을 vname, vage 변수에 저장합니다.
+	DBMS_OUTPUT.PUT_LINE(CHR(10));	-- 줄 바꿈
+	DBMS_OUTPUT.PUT_LINE('=고객이름 : ' || vname);	-- 줄 바꿈
+	DBMS_OUTPUT.PUT_LINE('=고객나이 : ' || vage);	-- 줄 바꿈
+END;
+	-- 줄 바꿈
+	
+

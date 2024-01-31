ec929fa0780d71e4cd8346aa948082bb3e7bf53e
hoony <qudgns0250@gmail.com>
2024-01-31 10:49:24 +0900

    dao

--- jdbcEX/src/project/dao/TblBuyDao.java
+++ jdbcEX/src/project/dao/TblBuyDao.java
@@ -9,22 +9,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import javax.naming.spi.DirStateFactory.Result;
-
 import project.vo.BuyVo;
 import project.vo.CustomerBuyVo;
+import project.vo.ProductVo;
+
 
 public class TblBuyDao {
-
-    public static void main(String[] args) {
-        String customid = System.console().readLine();
-
-        TblBuyDao dao = new TblBuyDao();
-        List<CustomerBuyVo> result = dao.selectCustomerBuyList(customid);
-        System.out.println(result);
-    }
-
-    public static final String URL = "jdbc:oracle:thin:@//localhost:1521/xe";
+    public static final String URL ="jdbc:oracle:thin:@//localhost:1521/xe";
     public static final String USERNAME = "c##idev";
     private static final String PASSWORD = "1234";
 
@@ -32,91 +23,145 @@ public class TblBuyDao {
         return DriverManager.getConnection(URL, USERNAME, PASSWORD);
     }
 
-    //executeUpdate 메소드는 insert, update, delete 가 정상 실행(반영된 행 있으면) 되면 1을 리턴,
-    //                  특히 Update, delete 는 조건에 맞는 행이 없어서 반영된 행이 없으면 0을 리턴.
-    public int insert(BuyVo vo) {
-        String sql = "insert into tbl_buy(buy_idx,customId,Pcode,Quantity,buy_date) " +
-                "values (buy_pk_seq.nextval, ?, ?, ?, sysdate)";
-        int result=0;        
-        try (Connection connection = getConnection();
-                PreparedStatement pstmt = connection.prepareStatement(sql);) {
+    //executeUpdate 메소드는 insert,update,delete 가 정상 실행되면 1을 리턴,
+    //                      특히 update, delete는 조건에 맞는 행이 없어서 반영되는 행이 없으면 0을 리턴
 
-            pstmt.setInt(1, vo.getBuyIdx());
-            pstmt.setString(2, vo.getCustomid());
-            pstmt.setString(3, vo.getPcode());
-            pstmt.setInt(4, vo.getQuantity());
+    public int insert(BuyVo vo){
+        String sql = "insert into tbl_buy(buy_idx , customid, pcode,quantity,buy_date)\n" +
+                    "values (buy_pk_seq.nextval,?,?,?,sysdate)";
+        int result=0;
+        try (   //auto close
+            Connection connection = getConnection();    //1) 서버와의 연결
+            PreparedStatement pstmt = connection.prepareStatement(sql); //2) 연결된 서버로 실행할 SQL전달. 서버가 SQL컴파일
+        ){
+            //매개변수 바인딩
+            pstmt.setString(1, vo.getCustomid());
+            pstmt.setString(2, vo.getPcode());
+            pstmt.setInt(3, vo.getQuantity());
 
+            result=pstmt.executeUpdate();                                  //3) 연결된 서버에 실행 요청
+        } catch (SQLException e) {
+            //customid와 pcode는 참조테이블에 존재하는 값으로 안하면 무결성 위반 오류
+            System.out.println("insert 실행 예외 발생 : "+ e.getMessage());
+        }//finally 없음
+        return result;
+    }
+
+    
+
+    public int delete(int buy_idx){
+        String sql = "DELETE FROM tbl_buy tc WHERE buy_idx = ?";
+        int result=0;
+        try (   //auto close
+            Connection connection = getConnection();
+            PreparedStatement pstmt = connection.prepareStatement(sql);
+        ){
+            //매개변수 바인딩
+            pstmt.setInt(1, buy_idx);
             result=pstmt.executeUpdate();
-
+            //buy_idx 컬럼에 없는 값이면 오류는 아니고 update 반영한 행의 개수가 0이다.
         } catch (SQLException e) {
-            //customid 와 pcode 는 참조테이블에 존재하는 값이 아니면 무결성 위반 오류
-            System.out.println("구매하기 실행 예외 발생 : " + e.getMessage());
-        } // close는 자동으로 합니다. finally 없음
+            System.out.println("delete 실행 예외 발생 : "+ e.getMessage());
+        }//finally 없음
         return result;
     }
 
-    public int delete(int Buyidx) {
-        int result = 0;
-        String sql = "delete from tbl_custom where buy_idx = ?";
-        try (Connection connection = getConnection();
-                PreparedStatement pstmt = connection.prepareStatement(sql);) {
-            pstmt.setInt(1, Buyidx);
-            result = pstmt.executeUpdate();
+    
+    public int update(Map<String,Integer> map){//BuyVo vo){
+        String sql = "UPDATE tbl_buy SET quantity = ? WHERE buy_idx = ?";
+        int result=0;
+        try (
+            Connection connection = getConnection();
+            PreparedStatement pstmt = connection.prepareStatement(sql);
+        ){
+            pstmt.setInt(1, map.get("quantity"));
+            pstmt.setInt(2, map.get("buyidx"));
+            result=pstmt.executeUpdate();
+            //buy_idx 컬럼에 없는 값이면 오류는 아니고 update 반영한 행의 개수가 0이다.
         } catch (SQLException e) {
-            //buy_idx 컬럼에 없는 값이면 오류는 아니고 update 반영한 행의 개수가 0입니다
-            System.out.println("구매취소 실행 예외 발생 : " + e.getMessage());
-        } // close는 자동으로 합니다. finally 없음
+            System.out.println("update 실행 예외 발생 : "+ e.getMessage());
+        }
         return result;
     }
 
-    public int update(Map<String, Integer> arg) {
-        int result = 0;
-        String sql = "update tbl_buy set Quantity = ?  where buy_idx = ?";
-        try (Connection connection = getConnection();
-            PreparedStatement pstmt = connection.prepareStatement(sql);) {
-            pstmt.setInt(1, arg.get("Quantity"));
-            pstmt.setInt(2, arg.get("buy_idx"));
-            result = pstmt.executeUpdate();
-            //buy_idx 컬럼에 없는 값이면 오류는 아니고 update 반영한 행의 개수가 0입니다.
-        } catch (SQLException e) {
-            System.out.println("수량 변경 실행 예외 발생 : " + e.getMessage());
-        } // close는 자동으로 합니다. finally 없음
-        return result;
-    }
-
-    // MyPage 기능
-    public List<CustomerBuyVo> selectCustomerBuyList(String customid) {
+    //mapage기능의 메소드
+    public List<CustomerBuyVo> selectCustomerBuyList(String customerid){
         List<CustomerBuyVo> list = new ArrayList<>();
-        String sql = "SELECT BUY_IDX , tb.PCODE , PNAME , PRICE , QUANTITY , BUY_DATE \r\n" +
-                "FROM TBL_BUY tb \r\n" +
-                "JOIN TBL_PRODUCT tp \r\n" +
-                "ON tb.PCODE = tp.PCODE \r\n" +
-                "WHERE tb.CUSTOMID  =? " +
-                "ORDER BY BUY_DATE DESC ";
 
-        try (Connection conn = getConnection();
-                PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            pstmt.setString(1, customid);
-            ResultSet rs = pstmt.executeQuery();
-
-            while (rs.next()) {
-                CustomerBuyVo vo = new CustomerBuyVo(rs.getString(1),
-                        rs.getString(2),
-                        rs.getString(3),
-                        rs.getInt(4),
-                        rs.getInt(5),
-                        rs.getTimestamp(6));
-                list.add(vo);
+        String sql = "SELECT BUY_IDX, tb.PCODE , PNAME, PRICE, QUANTITY,  BUY_DATE \r\n" + 
+                "FROM TBL_BUY tb \r\n" + 
+                "JOIN TBL_PRODUCT tp \r\n" + 
+                "ON tb.PCODE = tp.PCODE \r\n" + 
+                "WHERE tb.CUSTOMID =?\r\n" + 
+                "ORDER BY BUY_DATE DESC";
+        try (
+            Connection connection = getConnection();
+            PreparedStatement pstmt = connection.prepareStatement(sql);
+        ){
+            pstmt.setString(1, customerid);
+            ResultSet ps = pstmt.executeQuery();
+            
+            
+            while (ps.next()) {
+                list.add(new CustomerBuyVo(ps.getInt(1),
+                                        ps.getString(2),
+                                        ps.getString(3),
+                                        ps.getInt(4),
+                                        ps.getInt(5),
+                                        ps.getTimestamp(6)
+                                        ));
             }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+            
+            // System.out.println("select 결과 : "+list);
+            // dao 메소드에는 특별한 목적이 아니면 출력문 작성 안함
+        } catch (SQLException e) {
+            System.out.println("selectCustomerBuyList 예외 발생 : "+e.getMessage());
             e.printStackTrace();
         }
+
         return list;
     }
 
-    public void insert(String customerID, String pCODE, int pRICE) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'insert'");
+
+    // 장바구니 모두 구매
+    // ㄴ batch(배치)는 일괄처리 : 실행할 insert, update, delete 등의 데이터 저장 DML을 여러개 모아 두었다가 한 번에 실행시킵니다.
+    // ㄴ 트랜잭션 : 특정 요구사항에 대한 '하나의 기능'을 실행할 '여러 SQL 명령들'로 구성된 '작업단위'
+    //              ㄴ 예시 : cart에 저장된 상품 중 하나라도 참조 값이 없는 pcode가 있으면 rollback, 모두 정상이면 commit
+    //                        트랜잭션 commit 모드가 auto에서 수동으로 변경.
+    public int insertMany(List<BuyVo> cart){    //여러번의 insert 반복 실행
+        String sql = "insert into tbl_buy\n" +  // 위의 insert 복붙하세요.
+                    "values (buy_pk_seq.nextval,?,?,?,sysdate)";
+        Connection connection = null;
+        PreparedStatement pstmt = null;
+        int count=0;
+        try {
+            connection = getConnection();    
+            pstmt = connection.prepareStatement(sql);
+            connection.setAutoCommit(false);    // ※ auto 커밋 해제
+            for(BuyVo vo : cart){
+                pstmt.setString(1, vo.getCustomid());
+                pstmt.setString(2, vo.getPcode());
+                pstmt.setInt(3, vo.getQuantity());
+                pstmt.addBatch();       // ※ sql을 메모리에 모아두기
+                count++;
+            }
+
+            pstmt.executeBatch();   // ※ 모아둔 sql을 일괄 실행하기. 실행 중에 무결성 오류
+            connection.commit();    //      catch에서 rollback
+        } catch (SQLException e) {      // 예외발생 : 트랜잭션 처리
+            try {
+                connection.rollback();
+                
+            } catch (SQLException e1) {   }
+            count =-1;
+                System.out.println("구매 불가능한 상품이 있습니다.");
+                System.out.println("장바구니 구매 실행 예외 발생 : "+e.getMessage());
+        }finally{       // 정상 실행과 예외 모두에 대해 자원 해제
+            try {       //트랜잭션  처리를 위해 connection을 사용해야 하므로 직접 close했습니다
+                pstmt.close();
+                connection.close();
+            }catch (SQLException e1) {}
+        }
+        return count;
     }
 }

62779faeb66e02ffacc3f84302488a99f2c07ec9
hoony <qudgns0250@gmail.com>
2024-01-31 11:33:42 +0900

    jdbc day 3

--- jdbcEX/src/jdbc/day3/CartApp_4.java
+++ jdbcEX/src/jdbc/day3/CartApp_4.java
@@ -13,7 +13,7 @@ import project.vo.ProductVo;
 //              ㄴ 단,  main 메소드도 제거하고 
 //                      인스턴스 메소드는 pivate 이외 다른  접근 한정자로 해야합니다.     
 public class CartApp_4 {
-    // 이 클래스의 전역변수 선언 - 모든 메소드에서 사용 가능. 인스턴스 필드와 같습니다.
+    //이 클래스의 전역변수 선언
     private TblBuyDao buyDao = new TblBuyDao();
     private TblProductDao productDao = new TblProductDao();
     private List<BuyVo> cart = new ArrayList<>();       //장바구니
@@ -36,49 +36,64 @@ public class CartApp_4 {
         System.out.println("카테고리 : A1-과일 A2-수입과일  B1-인스턴스  B2-선물세트 C1-과자류");
         System.out.print("카테고리 입력__");
         String category = System.console().readLine();
-                        List<ProductVo> productlist = productDao.selectByCategory(category);
-                        for(ProductVo vo : productlist)
-                            System.out.println(vo);
+        List<ProductVo> productlist = productDao.selectByCategory(category);
+        for(ProductVo vo : productlist)
+            System.out.println(vo);
+      
     }
 
     public void searchProductListByPname() {
         System.out.print("상품명 검색어 입력__");
         String pname = System.console().readLine();
         List<ProductVo> productlist = productDao.selectByPname(pname);
-                for(ProductVo vo : productlist)
-                        System.out.println(vo);
+        for(ProductVo vo : productlist)
+            System.out.println(vo);
     }                    
 
-    private void addCartItem(String customerid) {
+    private void addCartItem(String customerid) {   //장바구니 : 테이블에 직접 추가하지 않고 List변수 사용/////////////////////////
         System.out.println(".................. 장바구니 :: 물품 담기 ...................");
         System.out.print("구매할 상품코드 입력하세요.__");
         String pcode = System.console().readLine();
-        
         System.out.print("구매할 수량 입력하세요.__");
-        int quantity = Integer.parseInt(System.console().readLine());
+        int quntity = Integer.parseInt(System.console().readLine());
+        cart.add(new BuyVo(0, customerid, pcode, quntity, null));
 
-        BuyVo vo = new BuyVo(0, customerid, pcode, quantity, null);
-                    
-        buyDao.insert(vo);
-        if(buyDao.insert(vo)==1)
-        System.out.println("상품을 담았습니다.");
-        else            // 참조테이블에 없는 값 입력했을 때
-            System.out.println("상품코드 또는 고객아이디 오류 입니다.");
+        
      }
 
     private void showCartList() {
         System.out.println(".................. 장바구니 :: 목록 보기...................");
+        for(int i=0; i<cart.size(); i++)
+            System.out.println(String.format("번호 : %d 물품 : %s", i,cart.get(i)));    //리스트에서 i번째 가져오기
     }
 
     private void removeCartItem() {
         System.out.println(".................. 장바구니 :: 물품 삭제 ...................");
         System.out.print( "삭제할 번호 입력__");
+        int index = Integer.parseInt(System.console().readLine());
+        System.out.println("물품을 장바구니에서 삭제했습니다.");
+        if (index <0 || index >= cart.size()){
+            System.out.println("잘못된 번호 입니다");
+            return;
+        }
+        cart.remove(index);
         System.out.println("물품을 장바구니에서 삭제했습니다.");
     }
 
     private void buyCartItems() {
+        if (cart.size()==0) {
+            System.out.println("장바구니가 비었습니다. 물품을 담으세요");
+            return;
+        }
         System.out.println(".................. 장바구니 :: 물품 모두 구매 ...................");
+        int result = buyDao.insertMany(cart);
+        if (result>0) {
+            System.out.println("장바구니 물품 구매가 정상적으로 완료되었습니다.");
+            cart.clear();   //cart리스트 비우기
+        }else System.out.println("장바구니 물품 구매 실패");
     }
+
+//////////////////////////////////////
 
        //상품 목록을 선택한 카테고리에 대해 보여주기  (구매할 상품 조회)
        //또는 상품명으로 검색 (구매할 상품 조회)

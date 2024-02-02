0c1b03a68a19b61e22d99c0e0c643a322f26c139
dev-resetok <qudgns0250@gmail.com>
2024-02-02 09:18:35 +0900

    day 4

--- jdbcEX/src/jdbc/day4/StoredProcTest.java
+++ jdbcEX/src/jdbc/day4/StoredProcTest.java
@@ -2,18 +2,52 @@ package jdbc.day4;
 
 import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.sql.Types;
 
 import jdbc.day1.OracleConnectionUtil;
 
 public class StoredProcTest {
     public static void main(String[] args) {
-        exec_max_customer();
+        // exec_max_customer();
+        exec_money_of_day();
     }
     
+    private static void exec_money_of_day(){
+        System.out.println("고객 ID와 날짜를 입력하면 총 구매금액을 조회합니다.");
+        System.out.println("고객 ID 입력_");
+        String customid = System.console().readLine();
+
+        System.out.println("구매 날짜 입력_");
+        String buydate = System.console().readLine();   // 입력형식 yyyy-mm-dd
+
+        Connection connection = OracleConnectionUtil.getConnection();
+        String sql = "{ call money_of_day(?,?,?) }";
+        try(
+            CallableStatement cstmt = connection.prepareCall(sql)  
+            ) {
+            // 프로시저의 IN 매개 변수 값 전달
+            cstmt.setString(1, customid); 
+            cstmt.setString(2, buydate); 
+            
+            // 프로시저 OUT 매개변수 1) 타입 설정 
+            cstmt.registerOutParameter(3, Types.NUMERIC);
+            cstmt.executeUpdate();  // 프로시저 실행
+            // OUT 매개변수 2) 결과 값 가져오기 : getXXX
+             
+            
+            String result = String.format("%s 고객님이 %s 에 구매한 총 구매금액 : %,8d ",
+                                                    customid,buydate,cstmt.getInt(3));
+            System.out.println(result);
+
+        } catch (SQLException e) {
+            System.out.println("money_of_day 프로시저 실행 예외 : " + e.getMessage());
+        }
+    }
+
     private static void exec_max_customer(){
         Connection connection = OracleConnectionUtil.getConnection();
-        String sql = "{call max_custom(?,?)}";        
+        String sql = "{ call max_custom(?,?) }";        
         // 프로시저 sql 형식 "{ call 프로시저 이름(?,?...) }"   매개변수는 정의된 개수만큼 ? 나열 
         try(
             CallableStatement cstmt = connection.prepareCall(sql)   
